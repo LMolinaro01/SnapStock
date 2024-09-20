cf8be6519959908004febc653c58cd68356796b9
Leonardo Molinaro <126402616+LMolinaro01@users.noreply.github.com>
2024-09-20 15:56:46 -0300

    Navegação entre telas e Formulário de Login

--- app.js
+++ app.js
@@ -1,103 +1,115 @@
-// Exercicio feito em aula, mudança de telas (Código Temporário)
-
-//STACK, TAB, DRAWER e PROTEGIDA
-
 import { useState } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, TextInput, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
- 
+
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
- 
-const Login = ({ navigation, route }) => {
+
+const PreLogin = ({ navigation, route }) => {
   return (
     <View>
-      <Button onPress={() => route.params.funcLogar(true)} title="Logar" />
-      <Button
-        onPress={() => navigation.navigate('Registrar')}
-        title="Registrar"
-      />
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Image
+          source={require('logo.png')}
+          style={{ width: 140, height: 110, marginBottom: 20 }}
+        />
+      </View>
+      
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => navigation.navigate('Registrar')} title="Registrar" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => navigation.navigate('Login')} title="Logar" />
+      </View>
+      
+      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20 }}> 
+        <Text>© Leonardo Molinaro</Text>
+      </View>
     </View>
   );
 };
- 
+
+const FormularioLogin = ({ route }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Login para testes
+    if (username === 'admin' && password === '123') {
+      route.params.funcLogar(true); // Simula o login bem-sucedido
+    } else {
+      alert('Credenciais incorretas');
+    }
+  };
+
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Nome de Usuário</Text>
+      <TextInput
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <Text>Senha</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      
+      <View style={styles.buttonContainer}>
+        <Button onPress={handleLogin} title="Entrar" />
+      </View>
+    </View>
+  );
+};
+
 const Registrar = () => {
   return <Text>Registrar</Text>;
 };
- 
-const Avisos = () => {
-  return <Text>Avisos</Text>;
-};
- 
-const Perfil = ({ navigation, route }) => {
+
+const Perfil = () => {
   return (
     <View>
-      <Text>Perfil legal</Text>      {' '}
-     
+      <Text>Perfil legal</Text>
     </View>
   );
 };
- 
+
 const Home = ({ navigation, route }) => {
   return (
     <View>
-           {' '}
-      <Button onPress={() => navigation.navigate('Perfil')} title="Perfil" />   {' '}
-      <Button onPress = {()=>route.params.funcLogar(false)} title='Ir para Tela de login'/>
+    
     </View>
   );
 };
- 
+
 const Config = () => {
   return <Text>Config</Text>;
 };
- 
-const Contatos = () => {
-  return <Text>Contatos</Text>;
+
+const Exibir = () => {
+  return <Text>Exibir</Text>;
 };
- 
+
 const Fotos = () => {
   return <Text>Foto legal</Text>;
 };
- 
+
 const App = () => {
   const [EstaLogado, setLogado] = useState(false);
   return EstaLogado ? (
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen
-          name="Home"
-          component={() => {
-            return (
-              <Tab.Navigator>
-                <Tab.Screen
-                  name="Home_tab"
-                  options={{ headerShown: false }}
-                  component={() => {
-                    return (
-                      <Stack.Navigator>
-                        <Stack.Screen
-                          name="Home"
-                          options={{ headerShown: false }}
-                          component={Home}
-                          initialParams={{ funcLogar: setLogado }}
-                        />
-                        <Stack.Screen name="Perfil" component={Perfil} />
-                      </Stack.Navigator>
-                    );
-                  }}
-                />
-                <Tab.Screen name="Avisos" component={Avisos} />
-              </Tab.Navigator>
-            );
-          }}
+          name="Início"
+          component={() => {}}
         />
         <Drawer.Screen name="Config" component={Config} />
-        <Drawer.Screen name="Contatos" component={Contatos} />
+        <Drawer.Screen name="Exibir" component={Exibir} />
         <Drawer.Screen name="Fotos" component={Fotos} />
       </Drawer.Navigator>
     </NavigationContainer>
@@ -105,8 +117,12 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
+          name="SnapStock"
+          component={PreLogin}
+        />
+        <Stack.Screen
           name="Login"
-          component={Login}
+          component={FormularioLogin}
           initialParams={{ funcLogar: setLogado }}
         />
         <Stack.Screen name="Registrar" component={Registrar} />
@@ -115,5 +131,17 @@ const App = () => {
     </NavigationContainer>
   );
 };
- 
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginVertical: 10, // Define o espaçamento vertical entre os botões
+  },
+  input: {
+    height: 40, 
+    borderColor: '#bcbab9', 
+    borderWidth: 1, 
+    marginBottom: 20,
+  }
+});
+
 export default App;
